4f222d6012f00dfe14947aafc539d077aaad3bc1
mateuszstacel <mateusz.stacel92@gmail.com>
2018-12-11 09:30:18 +0000

    added throwing error to console and describe option

--- squareSpec.js
+++ squareSpec.js
@@ -1,15 +1,24 @@
 function Expect(actual, expected) {
   if(actual != expected) {
     //Run if failed
-    document.getElementById("result").innerHTML = "Test Passed";
-    console.log("pass")
+    document.getElementById("result").innerHTML = "Test Fail";
+    throw {type: "Test Fail", details: {actual: actual, expected: expected}};
   } else {
     //Run if successful
-   document.getElementById("result").innerHTML = "Test Fail";
-     console.log("fail")
+   document.getElementById("result").innerHTML = "Test Pass";
+   throw {type: "Test passed", details: {actual: actual, expected: expected}};
   }
 };
 
+var describe = function(message, testFunc) {
+  try {
+    testFunc();
+  } catch(err) {
+    console.error(message, err);
+  }
+};
+
+describe("Checking square area", function() {
 square = new Square(2);
 Expect(square.area(), 1)
-console.log(square.area())
+});
